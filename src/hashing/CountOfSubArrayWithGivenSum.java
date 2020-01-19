b0c395e9f0bcca8ffc4124d4c9d34b214bc202a0
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-01-19 23:31:07 +0530

    Practiced hashing problems

--- src/hashing/CountOfSubArrayWithGivenSum.java
+++ src/hashing/CountOfSubArrayWithGivenSum.java
@@ -3,10 +3,10 @@ package hashing;
 import java.util.HashMap;
 import java.util.Map;
 
-public class SubArrayWithGivenSum {
+public class CountOfSubArrayWithGivenSum {
     public static void main(String[] args) {
-        int array[] = {8,3,1,5,-6,6,2,2};
-        int sumNeeded = 4;
+        int array[] = {8,3,7};
+        int sumNeeded = 15;
         System.out.println(isSubArrayWithZeroSumPresent(array, array.length, sumNeeded));
     }
 
@@ -15,17 +15,6 @@ public class SubArrayWithGivenSum {
         Map<Integer, Integer> map = new HashMap<>(array.length);
         int max =0;
         for (int i=0; i<array.length; i++) {
-            /*Adding the sum as if the sum is the given number at any contiguous position
-            then the sum goes back to the same number again
-            Eg -> array = {-5,-8,6,13,3,-1};
-                summationSet = {-5, -13, -7, 6, 9, 8}
-                We check: -> given num = 22
-                -5 - 22 = -27 not present in set
-                -13 - 22 = -35 not present in set
-                -7 - 22 = -29 not present in set
-                6 - 22 = -16 not present in set
-                9 - 22 = -13 PRESENT IN SET, therefore the summation number is present
-            */
             int calculatedMaximunIndexDifference = 0;
             sumOfArrayElements  += array[i];
             int differenceValue = sumOfArrayElements - sum;
@@ -34,7 +23,8 @@ public class SubArrayWithGivenSum {
                 calculatedMaximunIndexDifference = i + 1;
             else if(map.get(differenceValue) != null)
                 calculatedMaximunIndexDifference = i - map.get(differenceValue);
-            else if(map.get(sumOfArrayElements) == null)
+
+            if(map.get(sumOfArrayElements) == null)
                 map.put(sumOfArrayElements, i);
 
             if(calculatedMaximunIndexDifference > max)
