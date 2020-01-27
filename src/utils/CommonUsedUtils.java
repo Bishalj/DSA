96a3488aab7583b6f37754625bc17cd25f41c053
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-01-27 22:28:47 +0530

    Practiced KMP string searching algorithm

---
+++ src/utils/CommonUsedUtils.java
@@ -0,0 +1,10 @@
+package utils;
+
+public class CommonUsedUtils {
+
+    public static void displayArray(int arr[]){
+        for (int value : arr)
+            System.out.print(value + " ");
+        System.out.println();
+    }
+}
