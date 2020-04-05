a258ad4eb1b48e30d7b537cf6faf2f2a875de7b0
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-04-05 19:07:07 +0530

    Practiced heap problems

---
+++ src/heap/HeapMain.java
@@ -0,0 +1,32 @@
+package heap;
+
+public class HeapMain {
+
+    public static void main(String[] args) {
+        HeapOperations heapOperations = new HeapOperations(5);
+
+        heapOperations.insert(5);
+        heapOperations.display();
+        heapOperations.insert(10);
+        heapOperations.display();
+        heapOperations.insert(4);
+        heapOperations.display();
+        heapOperations.insert(12);
+        heapOperations.display();
+        heapOperations.insert(33);
+//        heapOperations.display();
+//        heapOperations.insert(33);
+        heapOperations.display();
+
+        System.out.println(heapOperations.getMinimumElement());
+        heapOperations.deleteTheMinimumElement();
+        heapOperations.display();
+
+        heapOperations.decreaseKeyOperation(4, 6);
+        heapOperations.display();
+        heapOperations.deleteElementAtGivenPosition(1);
+        int[] arr ={45,22,67,46,3,1,5,2};
+        arr = heapOperations.heapSort(arr);
+        heapOperations.display(arr);
+    }
+}
---
+++ src/heap/MaximumItemsWithGivenSum.java
@@ -0,0 +1,29 @@
+package heap;
+
+import java.util.PriorityQueue;
+
+public class MaximumItemsWithGivenSum {
+
+    public static void main(String[] args) {
+        int arr[] = {9,8,7,15,18};
+        int sum=40;
+
+        System.out.println(countOfMaximumItemsWithGivenSum(arr, sum));
+    }
+
+    private static int countOfMaximumItemsWithGivenSum(int[] arr, int sum) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        for (int i = 0; i < arr.length; i++) {
+            priorityQueue.add(arr[i]);
+        }
+
+        int count = 0;
+        while (!priorityQueue.isEmpty()){
+            sum = sum - priorityQueue.poll();
+            if(sum < 0)
+                return count;
+            count++;
+        }
+        return arr.length;
+    }
+}
