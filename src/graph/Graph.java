4d05e7c86b327d1873f878bccabc747e331b4217
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-04-07 00:07:51 +0530

    Practiced graph problems

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,6 +1,7 @@
 package graph;
 
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class Graph {
 
@@ -30,6 +31,57 @@ public class Graph {
         }
     }
 
+    void printBFSPath(int source){
+        final Queue<Integer> queue = new LinkedList<>();
+        final boolean[] visited = new boolean[size];
+        queue.add(source);
+        visited[source] = true;
+        System.out.println("-------------------- Print BFS with Source "+source+" ------------------------------");
+        while (queue.isEmpty() == false){
+            int vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            LinkedList<Integer> edges = adjacentList[vertex];
+            for (int i = 0; i < edges.size(); i++) {
+                int currentEdge = edges.get(i);
+                if(visited[currentEdge] == false){
+                    queue.add(currentEdge);
+                    visited[currentEdge] = true;
+                }
+            }
+        }
+        System.out.println();
+    }
+
+
+    void printBFSPathForDisconnectedGraph(){
+        boolean[] visited = new boolean[size];
+        for(int i=0; i<size; i++){
+            if(visited[i] == false)
+                printBFSPathForDisconnectedGraph(i, visited);
+        }
+    }
+
+    void printBFSPathForDisconnectedGraph(int source, boolean[] visited){
+        final Queue<Integer> queue = new LinkedList<>();
+        queue.add(source);
+        visited[source] = true;
+        System.out.println("-------------------- Print BFS with Source "+source+" ------------------------------");
+        while (queue.isEmpty() == false){
+            int vertex = queue.poll();
+            System.out.print(vertex + " ");
+
+            LinkedList<Integer> edges = adjacentList[vertex];
+            for (int i = 0; i < edges.size(); i++) {
+                int currentEdge = edges.get(i);
+                if(visited[currentEdge] == false){
+                    queue.add(currentEdge);
+                    visited[currentEdge] = true;
+                }
+            }
+        }
+        System.out.println();
+    }
 
 
 }
