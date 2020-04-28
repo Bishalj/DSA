bd69dbbfa0e2a0ec3504764c846e255c437b32ef
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-04-28 16:53:12 +0530

    practiced dynamic  programming

---
+++ src/dynamic_programming/LongestIncreasingSubsequence.java
@@ -0,0 +1,27 @@
+package dynamic_programming;
+
+public class LongestIncreasingSubsequence {
+
+    public static void main(String[] args) {
+        int arr[] = {3,7,2,3,4,5,11,9,10};
+        int index = 0;
+        int lastValue = Integer.MIN_VALUE;
+        System.out.println(getTheLongestIncreasingSubsequence(arr, index, lastValue));
+    }
+
+    private static int getTheLongestIncreasingSubsequence(int[] arr, int index, int lastValue) {
+        if(index == arr.length)
+            return 0;
+
+
+        int lengthIncluding =0;
+        if(arr[index] > lastValue)
+            lengthIncluding = 1 + getTheLongestIncreasingSubsequence(arr, index+1, arr[index]);
+
+        int lengthExcluding = getTheLongestIncreasingSubsequence(arr, index+1, lastValue);
+        if(lengthExcluding > lengthIncluding)
+            return lengthExcluding;
+
+        return lengthIncluding;
+    }
+}
---
+++ src/java_practice/PracticeVolatile.java
@@ -0,0 +1,21 @@
+package java_practice;
+
+public class PracticeVolatile implements Runnable {
+
+    public volatile boolean flag = false;
+    public static void main(String[] args) throws InterruptedException {
+        Runnable target;
+        Thread t1 = new Thread("one");
+        t1.start();
+        t1.sleep(800);
+
+        Thread t2 = new Thread("two");
+        t2.start();
+        t1.sleep(200);
+    }
+
+    @Override
+    public void run() {
+        flag = true;
+    }
+}
