a8e93edebbc69d26d661bb08a4e97a47810438e3
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2020-05-28 08:48:05 +0530

    DP problem solving

---
+++ src/Arrays/ReverseTheBits.java
@@ -0,0 +1,32 @@
+package Arrays;
+
+public class ReverseTheBits {
+    public static int reverseBits(int n)
+    {
+        int rev = 0;
+
+        // traversing bits of 'n'
+        // from the right
+        while (n > 0)
+        {
+            // bitwise left shift
+            // 'rev' by 1
+            System.out.println("Before rev <<= 1 -> "+ rev);
+            rev <<= 1;
+            System.out.println("After rev <<= 1 -> "+ rev);
+            // if current bit is '1'
+            if ((int)(n & 1) == 1)
+                rev ^= 1;
+
+            // bitwise right shift
+            //'n' by 1
+            n >>= 1;
+        }
+        // required number
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseBits(11));
+    }
+}
---
+++ src/dynamic_programming/KnapSackZeroOneDP.java
@@ -0,0 +1,8 @@
+package dynamic_programming;
+
+public class KnapSackZeroOneDP {
+
+    public static void main(String[] args) {
+
+    }
+}
