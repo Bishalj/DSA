a0588e126c68d66e3a87746d7c20bbce1f7a40f9
Bishalj <jaiswalbishal3@gmail.com>
2021-04-17 01:07:05 +0530

    practice :

---
+++ src/gfg/LPSArray.java
@@ -0,0 +1,24 @@
+package gfg;
+
+import utils.CommonUsedUtils;
+
+public class LPSArray {
+
+    private static int[] getLPS(String str){
+        if(str.length() == 0)
+            return null;
+        int arr[] = new int[str.length()];
+
+        for(int i=1; i<str.length(); i++){
+            if(str.charAt(arr[i-1]) == str.charAt(i))
+                arr[i] = arr[i-1] + 1;
+        }
+        return arr;
+    }
+
+  public static void main(String[] args) {
+        String str = "abbabb";
+        int lps[] = getLPS(str);
+      CommonUsedUtils.displayArray(lps);
+  }
+}
