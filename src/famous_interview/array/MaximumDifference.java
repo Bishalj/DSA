7658c69e76f5e41578cb3dba7d66f79502bf5d00
Bishalj <jaiswalbishal3@gmail.com>
2021-08-18 01:09:07 +0530

    solved searching, sorting and linklist question

--- src/famous_interview/array/MaximumDifference.java
+++ src/famous_interview/array/MaximumDifference.java
@@ -1,5 +1,6 @@
 package famous_interview.array;
 
+
 public class MaximumDifference {
 
 	public static void main(String[] args) {
@@ -12,4 +13,35 @@ public class MaximumDifference {
 		}
 		System.out.println(diff);
 	}
+
+	public boolean isPalindrome(ListNode1 head) {
+		ListNode1 node = reverseList(head);
+		while(node != null){
+			if(head == null || node.val != head.val)
+				return false;
+			node = node.next;
+			head = head.next;
+		}
+		return true;
+	}
+
+	public ListNode1 reverseList(ListNode1 head) {
+		ListNode1 previous = null;
+		ListNode1 node = head;
+		while(node != null){
+			ListNode1 next = node.next;
+			node.next = previous;
+			previous = node;
+			node = next;
+		}
+		return previous;
+	}
 }
+
+ class ListNode1 {
+      int val;
+      ListNode1 next;
+      ListNode1() {}
+      ListNode1(int val) { this.val = val; }
+      ListNode1(int val, ListNode1 next) { this.val = val; this.next = next; }
+  }
