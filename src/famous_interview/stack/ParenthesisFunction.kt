36b931c74b6c59127eb1cbb9cac6c1cd5a40ab8e
Bishalj <jaiswalbishal3@gmail.com>
2022-01-18 19:36:15 +0530

    practices sorting

---
+++ src/famous_interview/stack/ParenthesisFunction.kt
@@ -0,0 +1,23 @@
+package famous_interview.stack
+
+class ParenthesisFunction {
+    fun main(){
+        val a  = mutableListOf<String>("a", "b", "c")
+
+        a.forEach {
+            it.printResult {
+                val data = it.plus(" Bishal")
+                data
+            }
+        }
+    }
+
+    public inline fun <T> Iterable<T>.forEach(action: (T) -> Unit): Unit {
+        for (element in this) action(element)
+    }
+
+    public inline fun <String> String.printResult(action : (String) -> kotlin.String): Unit{
+       val d =  action(this)
+        println(d)
+    }
+}
